77889237a4041c16c4a1b1d693433a2026c2294b
Ahmet Gökay Ürkmez <gokay.urkmez@tigahealth.com>
2023-12-25 02:55:25 +0300

    Solution of LeetCode partitioning into minimum number of deci-binary numbers
    -

---
+++ cpp/Deci_BinaryNumbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minPartitions(string str) {
+        int max = 0;
+        int num;
+
+        for(int i=0; i<str.length(); ++i) {
+            num = str[i] - '0';
+
+            if(num>max)
+                max = num;
+        }
+	
+        return max;
+    }
+};
+
+
+
+int main() {
+	Solution obj;
+	string str;
+	getline(cin, str);
+	obj.minPartitions(str);
+
+	return 0;
+}
